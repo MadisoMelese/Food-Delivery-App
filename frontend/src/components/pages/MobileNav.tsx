8882ee88970160cbbe50f0500419500cfb3f646e
Madiso Melese <madishamadiso00@gmail.com>
2025-02-05 23:40:03 +0300

    Mobile Nav updated

--- frontend/src/components/pages/MobileNav.tsx
+++ frontend/src/components/pages/MobileNav.tsx
@@ -1,4 +1,4 @@
-import { CircleUserRound, Menu } from "lucide-react"
+import {CircleUserRound, Menu } from "lucide-react"
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "../ui/sheet"
 import { Separator } from "@radix-ui/react-separator"
 import { useAuth0 } from "@auth0/auth0-react"
@@ -16,11 +16,10 @@ const MobileNav = () => {
       <SheetContent className="space-y-3">
         <SheetTitle>
           {isAuthenticated ?
-            (<span className="flex items-center font-bold gap-2">
-              <CircleUserRound
-                className="text-orange-500"
-              />
+            (<span className="flex items-center hover:text-orange-500 font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
               {user?.email}
+
             </span>) :
             (<span>Welcome To Eats House</span>)}
 
