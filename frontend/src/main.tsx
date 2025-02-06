815df913407b464a8ec8d299e4e3a88b5338bd23
Madiso Melese <madishamadiso00@gmail.com>
2025-02-06 06:22:31 +0300

    react-query added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
 import AppRoutes from './AppRoutes.tsx'
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      // staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <Auth0ProviderWithNavigate>
-    <AppRoutes />
-
-    </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
